283565ec1c35531672bf04926eb8e0e0e4313495
wajahat959 <wajahat.h@anemoia.dev>
2024-08-07 13:13:46 +0500

    pipes moving

--- app/index.js
+++ app/index.js
@@ -1,27 +1,62 @@
-import React from "react";
-import { Canvas, useImage,Image, SkImage } from "@shopify/react-native-skia";
-import {useWindowDimensions} from 'react-native'
+import React, { useEffect } from "react";
+import { Canvas, useImage, Image, SkImage } from "@shopify/react-native-skia";
+import { useWindowDimensions } from "react-native";
+import {
+  Easing,
+  useSharedValue,
+  withRepeat,
+  withSequence,
+  withTiming,
+} from "react-native-reanimated";
 const Index = () => {
-    const bg= useImage(require('@/assets/sprites/background-day.png'))
-    const bird =useImage(require('@/assets/sprites/yellowbird-upflap.png'))
-    const pipeBottom =useImage(require('@/assets/sprites/pipe-green.png'))
-    const pipeTop =useImage(require('@/assets/sprites/pipe-green-top.png'))
-    const base =useImage(require('@/assets/sprites/base.png'))
-    const {width,height} =useWindowDimensions()
- const pipeOffset=0;
-  return (
-    <Canvas style={{ width, height }} >
-    {/* bg */}
-    <Image image={bg} width={width} height={height} fit={'cover'} />
-    {/* pipe */}
-    <Image image={pipeTop} y={pipeOffset-320} x={width/2} height={640} width={104} />
-    <Image image={pipeBottom} y={height-320+pipeOffset} x={width/2} height={640} width={104} />
-  {/* base */}
-  <Image image={base}  width={width} height={100} y={height-75} x={0} fit={'cover'} /> 
-  {/* bird */}
-    <Image image={bird} y={height/2} x={width/4} width={64} height={48} />
-    {/* pipe */}
+  const bg = useImage(require("@/assets/sprites/background-day.png"));
+  const bird = useImage(require("@/assets/sprites/yellowbird-upflap.png"));
+  const pipeBottom = useImage(require("@/assets/sprites/pipe-green.png"));
+  const pipeTop = useImage(require("@/assets/sprites/pipe-green-top.png"));
+  const base = useImage(require("@/assets/sprites/base.png"));
+  const { width, height } = useWindowDimensions();
+  const x = useSharedValue(width);
+  const pipeOffset = 0;
 
+  useEffect(() => {
+    x.value = withRepeat(
+      withSequence(
+        withTiming(-100, { duration: 3000, easing: Easing.linear }),
+        withTiming(width, { duration: 0 })
+      ),
+      -1
+    );
+  }, []);
+  return (
+    <Canvas style={{ width, height }}>
+      {/* bg */}
+      <Image image={bg} width={width} height={height} fit={"cover"} />
+      {/* pipe */}
+      <Image
+        image={pipeTop}
+        y={pipeOffset - 320}
+        x={x}
+        height={640}
+        width={104}
+      />
+      <Image
+        image={pipeBottom}
+        y={height - 320 + pipeOffset}
+        x={x}
+        height={640}
+        width={104}
+      />
+      {/* base */}
+      <Image
+        image={base}
+        width={width}
+        height={110}
+        y={height - 70}
+        x={0}
+        fit={"cover"}
+      />
+      {/* bird */}
+      <Image image={bird} y={height / 2} x={width / 4} width={64} height={48} />
     </Canvas>
   );
 };
